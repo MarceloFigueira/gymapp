e99609437a36b5e3f1d1e0dd806b824ac61c29f1
Marcelo Figueira <marcelofigueira87@gmail.com>
2023-06-28 22:25:13 -0300

    created myColors

---
+++ lib/core/my_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class MyColors {
+  static const Color darkBlue = Color(0xff0A6D92);
+  static const Color topBlue = Color(0xff00ADFA);
+  static const Color bottomBlue = Color(0xffBFF9FF);
+}
